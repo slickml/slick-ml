f3b04d536a01de2825a2b97a8bdb9316b144bf8b
Amirhessam Tahmassebi <amir.benny@gmail.com>
2020-09-02 01:05:01 -0500

    Update setup.py ...

--- setup.py
+++ setup.py
@@ -1,22 +1,54 @@
+import io
+import os
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#==============================================================================
+# Utilities
+#==============================================================================
+def read(path, encoding='utf-8'):
+    path = os.path.join(os.path.dirname(__file__), path)
+    with io.open(path, encoding=encoding) as fp:
+        return fp.read()
+
+
+#==============================================================================
+# Variables
+#==============================================================================
+DESCRIPTION         = "SlickML is a Machine Learning Library for Python"
+LONG_DESCRIPTION    = read("README.md")
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
+NAME                = "slickml"
+AUTHOR              = "Amirhessam Tahmassebi, Trace Smith"
+AUTHOR_EMAIL        = "amir.benny@gmail.com, tsmith5151@gmail.com"
+URL                 = "http://www.slickml.com"
+DOWNLOAD_URL        = "https://github.com/slickml/slick-ml/"
+LICENSE             = "MIT"
+PYTHON_REQUIRES     = ">=3.6"
+PACKAGES            = setuptools.find_packages()
+VERSION             = "0.0.4.beta"
+
+#==============================================================================
+# Setup
+#==============================================================================
 
 setuptools.setup(
-    name="slickml",
-    version="0.0.4",
-    author="Amirhessam Tahmassebi, Trace Smith",
-    author_email="amir.benny@gmail.com, tsmith5151@gmail.com",
-    description="SlickML is a Machine Learning Library for Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    license="MIT",
-    classifiers=[
+      name=NAME,
+      version=VERSION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+      packages=PACKAGES,
+      license=LICENSE,
+      python_requires=PYTHON_REQUIRES,
+      classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English"
+        ],
+    )
